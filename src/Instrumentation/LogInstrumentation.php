4b17fa487179e2992dfbd68797e9c0696be812e4
Pradeep <webreinvent@gmail.com>
2025-05-04 02:26:04 +0530

    updated: log instrumentation with error levels

--- src/Instrumentation/LogInstrumentation.php
+++ src/Instrumentation/LogInstrumentation.php
@@ -6,9 +6,11 @@ use Illuminate\Log\Events\MessageLogged;
 use Illuminate\Support\Facades\Event;
 use WebReinvent\VaahSignoz\Exceptions\VaahSignozException;
 use WebReinvent\VaahSignoz\Tracer\TracerFactory;
+use OpenTelemetry\API\Trace\StatusCode;
 
 class LogInstrumentation
 {
+
     public function boot()
     {
         try {
@@ -20,10 +22,19 @@ class LogInstrumentation
 
     public function handleMessageLogged(MessageLogged $event)
     {
+
         $tracer = TracerFactory::getTracer();
-        $span = $tracer->spanBuilder('log.message')->startSpan();
+
+        $spanName = 'log.' . strtolower($event->level);
+        $span = $tracer->spanBuilder($spanName)->startSpan();
         $span->setAttribute('log.level', $event->level);
         $span->setAttribute('log.message', $event->message);
+
+        if (strtolower($event->level) === 'error') {
+            $span->setStatus(StatusCode::STATUS_ERROR, 'Error log');
+            $span->setAttribute('error', true);
+        }
+
         $span->end();
     }
 }
